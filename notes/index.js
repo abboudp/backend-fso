c25b23d44a1bed2991c324f0cccfe8611ce7f85c
Paul Abboud <abboudpaul112@gmail.com>
2023-02-09 15:45:19 -0800

    port change

--- notes/index.js
+++ notes/index.js
@@ -13,6 +13,8 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
@@ -93,7 +95,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
